44e7b28c8383791cacc8489f680a587de75011e7
RaynerLim <RavenNeo530@gmail.com>
2025-03-10 13:07:11 +0800

    update tests file within payment routes

--- payments/src/routes/__test__/new.test.ts
+++ payments/src/routes/__test__/new.test.ts
@@ -1,12 +1,10 @@
-import mongoose, { mongo } from "mongoose";
+import mongoose from "mongoose";
 import request from "supertest";
 import { OrderStatus } from "@novagenesis/common";
 import { app } from '../../app';
-import { Order } from "../../models/order";
-import { stripe } from "../../stripe";
-import { Payment } from "../../models/payment";
-
-//jest.mock('../../stripe');
+import { Order } from '../../models/order';
+import { stripe } from '../../stripe';
+import { Payment } from '../../models/payment';
 
 it('returns a 404 when purchasing an order that does not exist', async () => {
     await request(app)
@@ -25,9 +23,8 @@ it('returns a 401 when purchasing an order that doesnt belong to the user', asyn
         userId: new mongoose.Types.ObjectId().toHexString(),
         version: 0,
         price: 20,
-        status: OrderStatus.Created
+        status: OrderStatus.Created,
     });
-
     await order.save();
 
     await request(app)
@@ -38,27 +35,6 @@ it('returns a 401 when purchasing an order that doesnt belong to the user', asyn
             orderId: order.id,
         })
         .expect(401);
-});
-
-it('returns a 400 when purchasing a cancelled order', async () => {
-
-    const userId = new mongoose.Types.ObjectId().toHexString();
-
-    const order = Order.build({
-        id: new mongoose.Types.ObjectId().toHexString(),
-        userId,
-        version: 0,
-        price: 20,
-        status: OrderStatus.Cancelled
-    });
-
-    await order.save();
-
-    await request(app).post('/api/payments').set('Cookie', global.signin(userId)).send({
-        orderId: order.id,
-        token: 'asdlkfj',
-    })
-        .expect(400);
 });
 
 it('returns a 400 when purchasing a cancelled order', async () => {
